fe6b5a5c6139f590e025744a6bba9bf8781e628c
sudolibre <optout.breaker@gmail.com>
2016-12-27 14:20:14 -0500

    Initial Commit

---
+++ Retirement/Retirement.h
@@ -0,0 +1,19 @@
+//
+//  Retirement.h
+//  Retirement
+//
+//  Created by Jonathon Day on 12/27/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Retirement.
+FOUNDATION_EXPORT double RetirementVersionNumber;
+
+//! Project version string for Retirement.
+FOUNDATION_EXPORT const unsigned char RetirementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Retirement/PublicHeader.h>
+
+
